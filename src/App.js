81e6579fbe6df94102cbb7546332ded095f25cea
thomasfregene <fregzythomas@gmail.com>
2020-07-23 14:00:55 +0100

    Connecting register Component to Server

--- src/App.js
+++ src/App.js
@@ -35,8 +35,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) =>{
+    this.setState({data: {
+      id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
   }
 
   
@@ -97,7 +114,7 @@ class App extends Component {
        :(
          route === 'signin' 
          ?  <Signin onRouteChange={this.onRouteChange}/>
-         :  <Register onRouteChange={this.onRouteChange}/>
+         :  <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
        )
       
       }
